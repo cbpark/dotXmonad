89132881c50b1da302f7f1379fe6ebd22b318ce9
cbpark <cbpark@gmail.com>
2016-01-24 03:30:28 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+function backup {
+    if [ -e $HOME/$1 ]; then
+        echo "-- $1 found."
+        mv -v $HOME/$1 $HOME/"$1.old"
+    fi
+}
+
+if command -v xmonad 2>/dev/null; then
+    backup ".xmonad"
+    backup ".xmobarrc"
+    backup ".stalonearryrc"
+    git clone git@github.com:cbpark/dotXmonad.git $HOME/.xmonad
+    command -v xmobar >/dev/null 2>&1 || { echo "You have to install xmobar." >&2; exit 1;}
+    ln -sf $HOME/.xmonad/.xmobarrc $HOME/.xmobarrc
+    ln -sf $HOME/.xmonad/.stalonetrayrc $HOME/.stalonetrayrc
+    xmonad --recompile
+else
+    echo "-- xmonad is not found."
+    exit 1
+fi
---
+++ xmonad.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import           Control.Monad              (when)
+import           Data.Monoid                (All (..))
+import           System.IO
+
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks   (avoidStruts, manageDocks)
+import           XMonad.Hooks.ManageHelpers (doFullFloat, isFullscreen)
+import           XMonad.Layout.NoBorders    (smartBorders)
+import qualified XMonad.StackSet            as W
+import           XMonad.Util.EZConfig       (additionalKeys)
+import           XMonad.Util.Run            (spawnPipe)
+
+main :: IO ()
+main = do
+  xmproc <- spawnPipe "xmobar"
+
+  xmonad $ def
+    { manageHook = composeAll [ manageDocks
+                              , isFullscreen --> doFullFloat
+                              -- , className =? "Vlc" --> doFullFloat
+                              , manageHook def ]
+    , handleEventHook = evHook
+    , layoutHook = avoidStruts $ smartBorders (layoutHook def)
+    , logHook = dynamicLogWithPP xmobarPP
+      { ppOutput  = hPutStrLn xmproc
+      , ppTitle   = xmobarColor "#7cafc2" "" . shorten 100
+      , ppCurrent = xmobarColor "#f7ca88" "" . wrap "[" "]" }
+    , terminal = "urxvt"
+    , modMask = mod4Mask
+    , borderWidth        = 8
+    , normalBorderColor  = "#181818"
+    , focusedBorderColor = "#a1b56c"
+    } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn
+       "xscreensaver-command -lock; xset dpms force off")
+    , ((controlMask, xK_Print),  spawn "sleep 0.2; scrot -s")
+    , ((0, xK_Print),            spawn "scrot")
+    , ((controlMask, xK_Return), spawn "dmenu_run")
+    ]
+
+evHook :: Event -> X All
+evHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
+  st     <- getAtom "_NET_WM_STATE"
+  fullsc <- getAtom "_NET_WM_STATE_FULLSCREEN"
+  isFull <- runQuery isFullscreen win
+  let remove = 0
+      add    = 1
+      toggle = 2
+      ptype  = 4
+      action = head dat
+  when (typ == st && fromIntegral fullsc `elem` tail dat) $ do
+    when (action == add || (action == toggle && not isFull)) $ do
+      io $ changeProperty32 dpy win st ptype propModeReplace [fromIntegral fullsc]
+      fullFloat win
+    when (head dat == remove || (action == toggle && isFull)) $ do
+      io $ changeProperty32 dpy win st ptype propModeReplace []
+      tileWin win
+  return $ All False
+evHook _ = return $ All True
+
+fullFloat, tileWin :: Window -> X ()
+fullFloat w = windows $ W.float w (W.RationalRect 0 0 1 1)
+tileWin w = windows $ W.sink w
