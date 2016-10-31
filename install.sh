29496820d18906e278004a263eae98e7e6afcf04
cbpark <cbpark@gmail.com>
2016-10-31 01:30:07 +0100

    Modify repo url

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
+REPO=https://github.com/cbpark/dotXmonad.git
+DOTXMONAD=$HOME/.xmonad
+CONTRIB=$(ghc-pkg list | grep "xmonad-contrib")
+
 function backup_old {
     if [ -e $HOME/$1 ]; then
         echo "-- $1 found."
@@ -11,15 +15,12 @@ function check_or_die {
     command -v $1 >/dev/null 2>&1 || { echo "-- $1 is not found." >&2; exit 1; }
 }
 
-DOTXMONAD=$HOME/.xmonad
-CONTRIB=$(ghc-pkg list | grep "xmonad-contrib")
-
 if command -v xmonad >/dev/null 2>&1; then
     check_or_die xmobar
     [ "" == "$CONTRIB" ] && { echo "-- xmonad-contrib is not found."; exit 1; }
     backup_old ".xmonad"
     backup_old ".xmobarrc"
-    git clone git@github.com:cbpark/dotXmonad.git $DOTXMONAD
+    git clone $REPO $DOTXMONAD
     ln -sf $DOTXMONAD/xmobarrc $HOME/.xmobarrc
     xmonad --recompile
     [ ! -s $DOTXMONAD/xmonad.errors ] && { cat xmonad.errors; exit 1; }
