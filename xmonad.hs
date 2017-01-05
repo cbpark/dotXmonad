c5e7ce3e85e1bf6f87050d1e3844ea5b3f12e09b
cbpark <cbpark@gmail.com>
2017-01-05 23:06:58 +0900

    Keep the main function to be smaller

--- xmonad.hs
+++ xmonad.hs
@@ -23,41 +23,19 @@ main = do
                                   , isFullscreen --> doFullFloat
                                   -- , className =? "Vlc" --> doFullFloat
                                   , manageHook def ]
-        , handleEventHook = evHook
+        , handleEventHook = myEventHook
         , layoutHook = avoidStruts . smartBorders $ layoutHook def
-        , logHook = dynamicLogWithPP xmobarPP
-          { ppOutput  = hPutStrLn xmproc
-          , ppTitle   = xmobarColor "#6ca0a3" "" . shorten 50
-          , ppCurrent = xmobarColor "#d0bf8f" "" . wrap "[" "]"
-            -- , ppLayout  = const ""
-          , ppLayout = \layStr -> let ls = words layStr
-                                  in unwords $ if length ls > 2
-                                               then (tail . tail) ls
-                                               else ls
-          }
+        , logHook = myLogHook xmproc
         , terminal = "urxvt"
         , modMask = mod4Mask
         , focusFollowsMouse = False
         , borderWidth        = 5
         , normalBorderColor  = "#1f1f1f"
         , focusedBorderColor = "#6ca0a3"
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn
-           "xscreensaver-command -lock; xset dpms force off")
-        , ((controlMask, xK_Print),  spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print),            spawn "scrot")
-        , ((controlMask, xK_Return), spawn "dmenu_run")
-        , ((0, xK_F1),               spawn "amixer -D pulse sset Master toggle")
-        , ((0, xK_F2),               spawn "amixer -D pulse sset Master 5%-")
-        , ((0, xK_F3),               spawn "amixer -D pulse sset Master 5%+")
-        , ((0, xK_F5),               spawn "xbacklight -dec 10")
-        , ((0, xK_F6),               spawn "xbacklight -inc 10")
-        , ((0, xK_F7),               spawn "kb-light.py -")
-        , ((0, xK_F8),               spawn "kb-light.py +")
-        ]
+        } `additionalKeys` myKeys
 
-evHook :: Event -> X All
-evHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
+myEventHook :: Event -> X All
+myEventHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
     st     <- getAtom "_NET_WM_STATE"
     fullsc <- getAtom "_NET_WM_STATE_FULLSCREEN"
     isFull <- runQuery isFullscreen win
@@ -74,8 +52,37 @@ evHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
             io $ changeProperty32 dpy win st ptype propModeReplace []
             tileWin win
     return $ All False
-evHook _ = return $ All True
+myEventHook _ = return $ All True
 
 fullFloat, tileWin :: Window -> X ()
 fullFloat w = windows $ W.float w (W.RationalRect 0 0 1 1)
 tileWin w = windows $ W.sink w
+
+myLogHook :: Handle -> X ()
+myLogHook proc =
+    dynamicLogWithPP xmobarPP
+    { ppOutput  = hPutStrLn proc
+    , ppTitle   = xmobarColor "#6ca0a3" "" . shorten 50
+    , ppCurrent = xmobarColor "#d0bf8f" "" . wrap "[" "]"
+    -- , ppLayout  = const ""
+    , ppLayout = \layStr -> let ls = words layStr
+                            in unwords $ if length ls > 2
+                                         then (tail . tail) ls
+                                         else ls
+    }
+
+myKeys :: [((KeyMask, KeySym), X ())]
+myKeys =
+    [ ((mod4Mask .|. shiftMask, xK_z),
+       spawn "xscreensaver-command -lock; xset dpms force off")
+    , ((controlMask, xK_Print),  spawn "sleep 0.2; scrot -s")
+    , ((0, xK_Print),            spawn "scrot")
+    , ((controlMask, xK_Return), spawn "dmenu_run")
+    , ((0, xK_F1),               spawn "amixer -D pulse sset Master toggle")
+    , ((0, xK_F2),               spawn "amixer -D pulse sset Master 5%-")
+    , ((0, xK_F3),               spawn "amixer -D pulse sset Master 5%+")
+    , ((0, xK_F5),               spawn "xbacklight -dec 10")
+    , ((0, xK_F6),               spawn "xbacklight -inc 10")
+    , ((0, xK_F7),               spawn "kb-light.py -")
+    , ((0, xK_F8),               spawn "kb-light.py +")
+    ]
